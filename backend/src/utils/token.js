ccc7abcdadc83dd703aa201a4db0cdddc4fcf10d
Akash Sharma <skysharma777@gmail.com>
2026-09-14 02:00:13 +0530

    feat: add access and refresh token utilities

---
+++ backend/src/utils/token.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken"
+
+const ACCESS_TOKEN_EXPIRY = "15m"
+const REFRESH_TOKEN_EXPIRY = "7d"
+
+const generateAccessToken = (user) => {
+    return jwt.sign(
+        {
+            userId: user._id,
+            role: user.role
+        },
+        process.env.JWT_ACCESS_SECRET,
+        {
+            expiresIn: ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+
+const generateRefreshToken = (user) => {
+    return jwjt.sign(
+        {
+            userId: user._id,
+            sessionId,
+        },
+        process.env.JWT_REFRESH_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
+
+export { generateAccessToken, generateRefreshToken }
