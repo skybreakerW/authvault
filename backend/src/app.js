df9e9c50fd6cb211e50c652dfa23e961b0e8980d
Akash Sharma <skysharma777@gmail.com>
2026-09-16 10:11:58 +0530

    feat: add security headers with helmet

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,12 @@
 import express from "express"
+import helmet from "helmet"
 import authRouter from "./routes/auth.route.js"
 import cookieParser from "cookie-parser"
 import adminRouter from "./routes/admin.route.js"
 
 const app = express()
 
-
+app.use(helmet())
 app.use(express.json())
 app.use(cookieParser())
 
